9801bbb6c244f437fefe86eb6bbc6a8311e4c786
Shukhrat Mukimov <mukimov@gmail.com>
2022-05-08 18:35:56 +0200

    Rewrite deprecated cases

--- tag-neovim-lua/config/nvim/after/plugin/completion.lua
+++ tag-neovim-lua/config/nvim/after/plugin/completion.lua
@@ -68,13 +68,6 @@ cmp.setup.cmdline(":", {
 cmp.event:on("confirm_done",
              cmp_autopairs.on_confirm_done({map_char = {tex = ""}}))
 
--- Setup lspconfig.
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp
-                                                                     .protocol
-                                                                     .make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require("lspconfig")["tsserver"].setup {capabilities = capabilities}
-
 vim.keymap.set("i", "<C-j>",
                "vsnip#expandable() ? '<Plug>(vsnip-expand)' : '<C-j>'",
                {expr = true})
--- tag-neovim-lua/config/nvim/after/plugin/nvim-lightbulb.lua
+++ tag-neovim-lua/config/nvim/after/plugin/nvim-lightbulb.lua
@@ -1,6 +1,1 @@
-require"nvim-lightbulb".update_lightbulb {
-  sign = {enabled = true, priority = 10},
-  float = {enabled = true, text = "💡", win_opts = {}},
-  virtual_text = {enabled = true, text = "💡"},
-  status_text = {enabled = true, text = "💡", text_unavailable = ""}
-}
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
