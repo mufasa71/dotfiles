5e4fa84ae1a65147c9d92336547148f0e2657247
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2022-12-28 14:38:01 +0500

    refactor: nvim configs

--- tag-neovim-lua/config/nvim/lua/plugins.lua
+++ tag-neovim-lua/config/nvim/lua/plugins.lua
@@ -3,33 +3,35 @@ return require("packer").startup(function(use)
 
   use {"tpope/vim-fugitive"}
   use {"tpope/vim-rhubarb"}
-  use {"lewis6991/gitsigns.nvim"}
-
   use {"tpope/vim-sleuth", "tpope/vim-repeat"}
-
   use {
     "nvim-telescope/telescope.nvim",
     requires = {
       {"nvim-lua/plenary.nvim"}, {"nvim-telescope/telescope-github.nvim"}
     }
   }
+  use {"lewis6991/gitsigns.nvim"}
   use {"nvim-telescope/telescope-fzf-native.nvim", run = "make"}
-
-  use {"lukas-reineke/indent-blankline.nvim"}
+  use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup {
+        space_char_blankline = " ",
+        show_current_context = true,
+        show_current_context_start = false
+      }
+    end
+  }
   use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
   use {"nvim-treesitter/nvim-treesitter-textobjects"}
-
   use {
     "neovim/nvim-lspconfig", "williamboman/nvim-lsp-installer",
     "simrat39/rust-tools.nvim"
   }
-
   use {"kosayoda/nvim-lightbulb"}
-
   use {"onsails/lspkind-nvim"}
   use {"sbdchd/neoformat"}
   use {"jparise/vim-graphql", "hashivim/vim-terraform"}
-
   use {
     "folke/which-key.nvim",
     config = function() require("which-key").setup {} end
@@ -63,19 +65,8 @@ return require("packer").startup(function(use)
   use {"github/copilot.vim"}
   use {
     "echasnovski/mini.nvim",
-    branch = "stable",
-    requires = {{"JoosepAlviste/nvim-ts-context-commentstring"}},
     config = function()
-      require("mini.comment").setup {
-        hooks = {
-          pre = function()
-            require("ts_context_commentstring.internal").update_commentstring({})
-          end
-        }
-      }
       require("mini.jump").setup {}
-      require("mini.surround").setup {}
-      require("mini.pairs").setup {}
       require("mini.completion").setup {
         lsp_completion = {source_func = "omnifunc", auto_setup = false}
       }
@@ -88,6 +79,21 @@ return require("packer").startup(function(use)
     end
   }
   use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
+  use {
+    "numToStr/Comment.nvim",
+    config = function() require("Comment").setup() end
+  }
+  use({
+    "kylechui/nvim-surround",
+    config = function()
+      require("nvim-surround").setup({
+      })
+    end
+  })
+  use {
     "akinsho/toggleterm.nvim",
     config = function()
       require("toggleterm").setup {
@@ -96,8 +102,16 @@ return require("packer").startup(function(use)
       }
     end
   }
-
+  use {
+    "pwntester/octo.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim",
+      "kyazdani42/nvim-web-devicons"
+    },
+    config = function() require"octo".setup() end
+  }
   -- Theme
   use {"EdenEast/nightfox.nvim"}
   use {"rebelot/kanagawa.nvim"}
+  use {"folke/tokyonight.nvim", {branch = "main"}}
 end)
