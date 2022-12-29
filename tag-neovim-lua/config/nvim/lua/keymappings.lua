b63d86a0e6dc57b0ef5dc457bc110625596834b9
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2022-12-29 20:35:24 +0500

    refactor: terminal keymappings

--- tag-neovim-lua/config/nvim/lua/keymappings.lua
+++ tag-neovim-lua/config/nvim/lua/keymappings.lua
@@ -1,35 +1,37 @@
 local wk = require "which-key"
 local remap = vim.keymap.set
 
-remap("n", "k", "v:count == 0 ? 'gk' : 'k'", {expr = true})
-remap("n", "j", "v:count == 0 ? 'gj' : 'j'", {expr = true})
-remap("n", "Y", "y$", {noremap = true, silent = true})
+remap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
+remap("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
+remap("n", "Y", "y$", { noremap = true, silent = true })
 
 wk.register({
   name = "General",
   -- w = {"<esc>:w<cr>", "Write current buffer"}, -- conflicts with workspace
-  ["c."] = {":%s/\\<<C-r><C-w>\\>//g<Left><Left>", "Search and replace"},
+  ["c."] = { ":%s/\\<<C-r><C-w>\\>//g<Left><Left>", "Search and replace" },
   -- x = {"<esc>:x<cr>", "Write if changes and quite"},
-  q = {"<esc>:q<cr>", "Quit current buffer"},
-  Q = {"<esc>:q<cr>", "Quit all buffers"},
+  q = { "<esc>:q<cr>", "Quit current buffer" },
+  Q = { "<esc>:q<cr>", "Quit all buffers" },
 
-  ["[b"] = {"<cmd>:bprevious<cr>", "Previous buffer"},
-  ["]b"] = {"<cmd>:bnext<cr>", "Next buffer"},
-  ["[B"] = {"<cmd>:bfirst<cr>", "First buffer"},
-  ["]B"] = {"<cmd>:blast<cr>", "Last buffer"},
+  ["[b"] = { "<cmd>:bprevious<cr>", "Previous buffer" },
+  ["]b"] = { "<cmd>:bnext<cr>", "Next buffer" },
+  ["[B"] = { "<cmd>:bfirst<cr>", "First buffer" },
+  ["]B"] = { "<cmd>:blast<cr>", "Last buffer" },
 
   x = {
     name = "Trouble",
-    x = {"<cmd>Trouble<cr>", "Open the list"},
-    w = {"<cmd>Trouble workspace_diagnostics<cr>", "Open workspace"},
-    d = {"<cmd>Trouble document_diagnostics<cr>", "Open diagnostics"},
-    l = {"<cmd>Trouble loclist<cr>", "Open location list"},
-    q = {"<cmd>Trouble quickfix<cr>", "Open quickfix list"},
-    R = {"<cmd>Trouble lsp_references<cr>", "Open quickfix list"}
+    x = { "<cmd>Trouble<cr>", "Open the list" },
+    w = { "<cmd>Trouble workspace_diagnostics<cr>", "Open workspace" },
+    d = { "<cmd>Trouble document_diagnostics<cr>", "Open diagnostics" },
+    l = { "<cmd>Trouble loclist<cr>", "Open location list" },
+    q = { "<cmd>Trouble quickfix<cr>", "Open quickfix list" },
+    R = { "<cmd>Trouble lsp_references<cr>", "Open quickfix list" }
   },
 
-  r = {"<cmd>NvimTreeRefresh<cr>", "Tree refresh"},
-  n = {"<cmd>NvimTreeFindFile<cr>", "Find file in tree"}
-}, {prefix = "<leader>"})
+  r = { "<cmd>NvimTreeRefresh<cr>", "Tree refresh" },
+  n = { "<cmd>NvimTreeFindFile<cr>", "Find file in tree" },
+  s = { "<cmd>YarnStart<cr>", "Run dev server" },
+  T = { "<cmd>YarnTest<cr>", "Run test" }
+}, { prefix = "<leader>" })
 
-wk.register({["<C-n>"] = {"<cmd>NvimTreeToggle<cr>", "Open tree"}});
+wk.register({ ["<C-n>"] = { "<cmd>NvimTreeToggle<cr>", "Open tree" } });
--- tag-neovim-lua/config/nvim/lua/terminal.lua
+++ tag-neovim-lua/config/nvim/lua/terminal.lua
@@ -1,11 +1,6 @@
 local Terminal = require("toggleterm.terminal").Terminal
-local yarn_start = Terminal:new({cmd = "yarn start", hidden = true})
-local yarn_test = Terminal:new({cmd = "yarn test --watch", hidden = true})
+local YarnTest = Terminal:new({ cmd = "yarn test --watch", hidden = true })
+local YarnStart = Terminal:new({ cmd = "yarn start", hidden = true })
 
-function _yarn_start_toggle() yarn_start:toggle() end
-function _yarn_test_toggle() yarn_test:toggle() end
-
-vim.api.nvim_set_keymap("n", "<leader>s", "<cmd>lua _yarn_start_toggle()<CR>",
-                        {noremap = true, silent = true})
-vim.api.nvim_set_keymap("n", "<leader>t", "<cmd>lua _yarn_test_toggle()<CR>",
-                        {noremap = true, silent = true})
+vim.api.nvim_create_user_command('YarnStart', function() YarnStart:toggle() end, {})
+vim.api.nvim_create_user_command('YarnTest', function() YarnTest:toggle() end, {})
