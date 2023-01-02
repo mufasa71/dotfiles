2d4eb5527bfea66d6cc1d3d5bddc18b85cb81799
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2023-01-02 12:10:37 +0500

    feat: add terminal commands

--- tag-neovim-lua/config/nvim/lua/terminal.lua
+++ tag-neovim-lua/config/nvim/lua/terminal.lua
@@ -1,6 +1,12 @@
 local Terminal = require("toggleterm.terminal").Terminal
 local YarnTest = Terminal:new({ cmd = "yarn test --watch", hidden = true })
 local YarnStart = Terminal:new({ cmd = "yarn start", hidden = true })
+local YarnInstall = Terminal:new({ cmd = "yarn install", hidden = true })
+local GithubRepoSync = Terminal:new({ cmd = "gh repo sync", hidden = true })
+local GithubCleanBranches = Terminal:new({ cmd = "gh clean-branches --force", hidden = true })
 
 vim.api.nvim_create_user_command('YarnStart', function() YarnStart:toggle() end, {})
 vim.api.nvim_create_user_command('YarnTest', function() YarnTest:toggle() end, {})
+vim.api.nvim_create_user_command('YarnInstall', function() YarnInstall:toggle() end, {})
+vim.api.nvim_create_user_command('GithubRepoSync', function() GithubRepoSync:toggle() end, {})
+vim.api.nvim_create_user_command('GithubCleanBranches', function() GithubCleanBranches:toggle() end, {})
