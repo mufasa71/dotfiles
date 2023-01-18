d024a938038e24844637cee5f00a81b6ab944cff
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2023-01-18 09:18:32 +0100

    [BREAKING]: remove mercurial aliases

--- tag-zsh/zsh/aliases.zsh
+++ tag-zsh/zsh/aliases.zsh
@@ -7,45 +7,10 @@ alias curl='noglob curl'
 alias youtube-dl='noglob youtube-dl'
 alias gpg='noglob gpg'
 
-# Mercurial
-alias hgc='hg commit'
-alias hgb='hg branch'
-alias hgba='hg branches'
-alias hgbk='hg bookmarks'
-alias hgco='hg checkout'
-alias hgd='hg diff'
-alias hged='hg diffmerge'
-# pull and update
-alias hgi='hg incoming'
-alias hgl='hg pull -u'
-alias hglr='hg pull --rebase'
-alias hgo='hg outgoing'
-alias hgp='hg push'
-alias hgs='hg status'
-alias hgsl='hg log --limit 20 --template "{node|short} | {date|isodatesec} | {author|user}: {desc|strip|firstline}\n" '
-alias hgca='hg commit --amend'
-# list unresolved files (since hg does not list unmerged files in the status command)
-alias hgun='hg resolve --list'
-
-function hgic() {
-    hg incoming "$@" | grep "changeset" | wc -l
-}
-
-function hgoc() {
-    hg outgoing "$@" | grep "changeset" | wc -l
-}
-
 function fe() {
   local files
   IFS=$'\n' files=($(fzf-tmux --query="$1" --multi --select-1 --exit-0))
   [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
-}
-
-function fbr() {
-  local branches branch
-  branches=$(hg branches -vv) &&
-	branch=$(echo "$branches" | fzf +m) &&
-	hg up $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
 }
 
 alias l='ls --color -1A' # Lists in one column, hidden files.
