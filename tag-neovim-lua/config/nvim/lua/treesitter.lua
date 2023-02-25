e6f958bfab9c911d33a0255016b50bc9abb2b1d4
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2023-02-25 15:13:45 +0100

    feat: treesitter add more languages and text obj

--- tag-neovim-lua/config/nvim/lua/treesitter.lua
+++ tag-neovim-lua/config/nvim/lua/treesitter.lua
@@ -1,8 +1,12 @@
 -- Parsers must be installed manually via :TSInstall
 require("nvim-treesitter.configs").setup {
   autoinstall = true,
-  ensure_installed = { "c", "lua", "rust", "typescript", "javascript", "css", "bash" },
-  highlight = { enable = true, additional_vim_regex_highlighting = false },
+  ensure_installed = {
+    "c", "lua", "rust", "typescript", "javascript", "css", "bash", "tsx",
+    "json", "markdown", "jsdoc", "html", "yaml", "dockerfile", "jenkinsfile",
+    "terraform"
+  },
+  highlight = {enable = true, additional_vim_regex_highlighting = false},
   incremental_selection = {
     enable = true,
     keymaps = {
@@ -12,7 +16,7 @@ require("nvim-treesitter.configs").setup {
       node_decremental = "grm"
     }
   },
-  indent = { enable = true },
+  indent = {enable = true},
   textobjects = {
     select = {
       enable = true,
@@ -41,14 +45,36 @@ require("nvim-treesitter.configs").setup {
     move = {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = { ["]m"] = "@function.outer", ["]]"] = "@class.outer" },
-      goto_next_end = { ["]M"] = "@function.outer", ["]["] = "@class.outer" },
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = {query = "@class.outer", desc = "Next class start"},
+        --
+        -- You can use regex matching and/or pass a list in a "query" key to group multiple queires.
+        ["]o"] = "@loop.*",
+        -- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+        --
+        -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+        -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+        ["]s"] = {query = "@scope", query_group = "locals", desc = "Next scope"},
+        ["]z"] = {query = "@fold", query_group = "folds", desc = "Next fold"}
+      },
+      goto_next_end = {["]M"] = "@function.outer", ["]["] = "@class.outer"},
       goto_previous_start = {
         ["[m"] = "@function.outer",
         ["[["] = "@class.outer"
       },
-      goto_previous_end = { ["[M"] = "@function.outer", ["[]"] = "@class.outer" }
+      goto_previous_end = {["[M"] = "@function.outer", ["[]"] = "@class.outer"}
+      -- Below will go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      -- Make it even more gradual by adding multiple queries and regex.
+      -- goto_next = {["]d"] = "@conditional.outer"},
+      -- goto_previous = {["[d"] = "@conditional.outer"}
+    },
+    swap = {
+      enable = true,
+      swap_next = {["<leader>a"] = "@parameter.inner"},
+      swap_previous = {["<leader>A"] = "@parameter.inner"}
     }
   },
-  context_commentstring = { enable = true, enable_autocmd = false }
+  context_commentstring = {enable = true, enable_autocmd = false}
 }
