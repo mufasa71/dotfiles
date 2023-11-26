fbc8c7d6a0fb2cd71629c7c79384b96f1dafae62
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2023-11-26 17:06:33 +0500

    add zsh

---
+++ .chezmoiscripts/run_onchange_install_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo pacman -S \
+	ripgrep \
+	ttf-jetbrains-mono \
+	ttf-jetbrains-mono-nerd \
+	zoxide \
+	fzf
--- run_onchange_install_packages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo pacman -S ripgrep ttf-jetbrains-mono ttf-jetbrains-mono-nerd
-
