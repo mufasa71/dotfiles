527b6efeed454d59212e5cc22c350f314079d7ef
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2024-02-01 10:45:33 +0100

    zsh changes :frog:

--- dot_config/zsh/config.zsh
+++ dot_config/zsh/config.zsh
@@ -6,8 +6,4 @@ alias cat=bat
 alias ls=eza
 alias rm='echo "This is not the command you are looking for."; false'
 
-[ -f /usr/share/fzf/completion.zsh ] && source /usr/share/fzf/completion.zsh
-[ -f /usr/share/fzf/key-bindings.zsh ] && source /usr/share/fzf/key-bindings.zsh
-
-eval "$(zoxide init zsh)"
-gpg-connect-agent updatestartuptty /bye >/dev/null
+bindkey '^X' create_completion
